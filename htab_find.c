3d365d9a6779693fd740b234087a4941dabb627d
Milan Jakubec <jakubeclp@gmail.com>
2023-04-11 15:13:02 +0200

    htab_find.c prep

---
+++ htab_find.c
@@ -0,0 +1,39 @@
+
+
+#include "htab.h"
+#include "htab_private.h"
+
+//Vraci ukazatel na zaznam s klicem key, pokud ho v tabulce neni, vraci NULL
+htab_pair_t * htab_find(const htab_t * t, htab_key_t key) {
+    size_t index = htab_hash_function(key) % t->arr_size; // index do pole ukazatelu
+    struct htab_item *item = t->arr_ptr[index]; 
+    while (item != NULL) { // hledani shody
+        if (strcmp(item->pair.key, key) == 0) {
+            return &item->pair; // shoda nalezena
+        }
+        item = item->next;
+    }
+    return NULL; // shoda nenalezena
+}
+
+/*
+      +----------+
+      | size     | // aktuální počet záznamů [(key,data),next]
+      +----------+
+      | arr_size | // velikost pole ukazatelů (počet položek)
+      +----------+
+      | arr_ptr  | // ukazatel na dynamicky alokované pole ukazatelů
+      +----------+
+        |
+        V
+      +---+
+      |ptr|-->[(key,data),next]-->[(key,data),next]-->[(key,data),next]--|
+      +---+
+      |ptr|--|
+      +---+
+      |ptr|-->[(key,data),next]-->[(key,data),next]--|
+      +---+
+      |ptr|--|
+      +---+
+      
+*/
