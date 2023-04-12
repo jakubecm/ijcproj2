6189ce98d5c4b30ec6d43af4402a6e8f96acc2ce
Milan Jakubec <jakubeclp@gmail.com>
2023-04-12 13:06:19 +0200

    htab_statistics.c prep

--- htab_erase.c
+++ htab_erase.c
@@ -8,20 +8,20 @@
 
 bool htab_erase(htab_t * t, htab_key_t key) {
     size_t index = htab_hash_function(key) % t->arr_size;
+    struct htab_item *previous = NULL; // pomocny ukazatel na predchozi prvek
     struct htab_item *item = t->arr_ptr[index]; // ukazatel na prvni prvek v seznamu
-    struct htab_item *prev = NULL; // pomocny ukazatel na predchozi prvek
     while (item != NULL) {
         if (strcmp(item->pair.key, key) == 0) {
-            if (prev == NULL) {
+            if (previous == NULL) {
                 t->arr_ptr[index] = item->next; // pokud je hledany prvek v seznamu, tak se posune ukazatel na prvni prvek v seznamu na dalsi prvek
             } else {
-                prev->next = item->next;
+                previous->next = item->next;
             }
             free(item); // uvolneni pameti konkretniho prvku
             t->size--; // zmena aktualniho obsazeni o jedna
             return true;
         }
-        prev = item;
+        previous = item;
         item = item->next; 
     }
     return false;
---
+++ htab_statistics.c
@@ -0,0 +1,55 @@
+
+#include "htab.h"
+#include "htab_private.h"
+#include <stdio.h>
+
+// výpočet a tisk statistik délky seznamů (min,max,avg) do stderr:
+void htab_statistics(const htab_t * t){
+    size_t min = 0; 
+    size_t max = 0;
+
+    for (size_t i = 0; i < t->arr_size; i++) {
+        size_t count = 0;
+        struct htab_item *item = t->arr_ptr[i]; // konkretni seznam itemu
+
+        while (item != NULL) {
+            count++;
+            item = item->next;
+        }
+        // Count je spocitany, ted je cas porovnat
+        if (min == 0){
+            min = count; // pokud min jeste neni inicializovany, tak se nastavi na count prvniho seznamu
+        }
+        else if (count < min) {
+            min = count;
+        }
+        
+        if (count > max) {
+            max = count;
+        }
+    }
+    fprintf(stderr, "min: %zu, max: %zu, avg: %f", min, max, (t->size / t->arr_size));
+
+}
+
+/*
+      +----------+
+      | size     | // aktuální počet záznamů [(key,data),next]
+      +----------+
+      | arr_size | // velikost pole ukazatelů (počet položek)
+      +----------+
+      | arr_ptr  | // ukazatel na dynamicky alokované pole ukazatelů
+      +----------+
+        |
+        V
+      +---+
+      |ptr|-->[(key,data),next]-->[(key,data),next]-->[(key,data),next]--|
+      +---+
+      |ptr|--|
+      +---+
+      |ptr|-->[(key,data),next]-->[(key,data),next]--|
+      +---+
+      |ptr|--|
+      +---+
+      
+*/
